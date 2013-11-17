a0df2fa5dfb5a90783a73786e6a042e6f8f8333c
Tim <tim@atom.(none)>
2013-11-17 21:35:27 +0000

    Set up secret token for dev/live

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# http://stackoverflow.com/a/18563754/10245
+SymbolLibrary::Application.config.secret_token =
+  if Rails.env.development? or Rails.env.test?
+      ('x' * 30) # meets minimum requirement of 30 chars long
+  else
+      ENV['SECRET_TOKEN']
+  end

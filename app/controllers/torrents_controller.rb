ca7075ab73f8bb31f8eadaa4734fa8793f32f8e6
Tim Abell <tim@timwise.co.uk>
2013-12-05 20:46:38 +0000

    Torrent list and navigation
    Hard coded for now.

---
+++ app/controllers/torrents_controller.rb
@@ -0,0 +1,4 @@
+class TorrentsController < ApplicationController
+  protect_from_forgery with: :exception
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ SymbolLibrary::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#index'
 
+  resource :torrents
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

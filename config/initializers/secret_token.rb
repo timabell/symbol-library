ce7639a0a040ef0ad1448719f06d2870375757d4
Tim Abell <tim@timwise.co.uk>
2013-12-21 12:44:25 +0000

    prod debug, prod?

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,11 +1,13 @@
 # http://stackoverflow.com/a/18563754/10245
 SymbolLibrary::Application.config.secret_token =
   if Rails.env.development? or Rails.env.test?
+    puts '~~~~~~~~~~dev~~~~~~~~~~'
       ('x' * 30) # meets minimum requirement of 30 chars long
   else
+    puts '~~~~~~~~~~prod~~~~~~~~~~'
       ENV['SECRET_TOKEN']
   end
 puts 'app setup env:'
 puts ENV.inspect
-      puts "ENV['SECRET_TOKEN'] #{ENV['SECRET_TOKEN']}"
+puts "ENV['SECRET_TOKEN'] #{ENV['SECRET_TOKEN']}"
 puts

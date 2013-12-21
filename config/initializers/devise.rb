ed4aa9ccd8296f1a7760251d18017e2adcdf9742
Tim Abell <tim@timwise.co.uk>
2013-12-21 10:40:06 +0000

    Debug production devise config
    key not getting set on heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,10 +4,12 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
+  puts "setting devise secret"
   if Rails.env.development? or Rails.env.test?
       config.secret_key = ('x' * 128)
   else
       config.secret_key = ENV['DEVISE_SECRET_KEY']
+      puts "config.secret_key = #{ENV['DEVISE_SECRET_KEY']}"
   end
 
   # ==> Mailer Configuration
